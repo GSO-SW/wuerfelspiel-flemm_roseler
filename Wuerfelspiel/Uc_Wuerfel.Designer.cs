c5b3d2d83bf01ec547a2d3059f5afe33e7374371
Erik Flemm <erikf@gso.schule.koeln>
2022-04-27 10:08:50 +0200

    Usercontrol Wuerfel kann gewuerfelt werden und zeigt ergebnis

---
+++ Wuerfelspiel/Uc_Wuerfel.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Wuerfelspiel
+{
+    partial class Uc_Wuerfel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Uc_Wuerfel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Uc_Wuerfel";
+            this.Load += new System.EventHandler(this.Uc_Wuerfel_Load);
+            this.Click += new System.EventHandler(this.Uc_Wuerfel_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Uc_Wuerfel_Paint);
+            this.DoubleClick += new System.EventHandler(this.Uc_Wuerfel_DoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
